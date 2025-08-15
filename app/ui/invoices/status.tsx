ce25eb1f8a3fc65b9764664a3d1221f3dfcd56c8
1rko <irkoabc@gmail.com>
2025-08-15 23:47:02 +0300

    Inishial commit

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -27,3 +27,20 @@ export default function InvoiceStatus({ status }: { status: string }) {
     </span>
   );
 }
+
+/*
+import clsx from 'clsx';
+
+export default function InvoiceStatus({ status }: { status: string }) {
+  return (
+    <span
+      className={clsx(
+        'inline-flex items-center rounded-full px-2 py-1 text-sm',
+        {
+          'bg-gray-100 text-gray-500': status === 'pending',
+          'bg-green-500 text-white': status === 'paid',
+        },
+      )}
+    >
+    // ...
+)}*/
